a9e85de49bdde89bd57bc3564001303e01dd46b9
Mikolaj <126107752+MeMikolaj@users.noreply.github.com>
2024-09-18 17:01:13 +1000

    Add files via upload

---
+++ env_utils.py
@@ -0,0 +1,64 @@
+from typing import Dict, List
+
+from trajdata.dataset_specific import RawDataset
+
+
+def get_raw_dataset(dataset_name: str, data_dir: str) -> RawDataset:
+    if "nusc" in dataset_name:
+        from trajdata.dataset_specific.nusc import NuscDataset
+
+        return NuscDataset(dataset_name, data_dir, parallelizable=False, has_maps=True)
+
+    if "lyft" in dataset_name:
+        from trajdata.dataset_specific.lyft import LyftDataset
+
+        return LyftDataset(dataset_name, data_dir, parallelizable=True, has_maps=True)
+
+    if "eupeds" in dataset_name:
+        from trajdata.dataset_specific.eth_ucy_peds import EUPedsDataset
+
+        return EUPedsDataset(
+            dataset_name, data_dir, parallelizable=True, has_maps=False
+        )
+
+    if "sdd" in dataset_name:
+        from trajdata.dataset_specific.sdd_peds import SDDPedsDataset
+
+        return SDDPedsDataset(
+            dataset_name, data_dir, parallelizable=True, has_maps=False
+        )
+
+    if "nuplan" in dataset_name:
+        from trajdata.dataset_specific.nuplan import NuplanDataset
+
+        return NuplanDataset(dataset_name, data_dir, parallelizable=True, has_maps=True)
+
+    if "waymo" in dataset_name:
+        from trajdata.dataset_specific.waymo import WaymoDataset
+
+        return WaymoDataset(dataset_name, data_dir, parallelizable=True, has_maps=True)
+
+    if "interaction" in dataset_name:
+        from trajdata.dataset_specific.interaction import InteractionDataset
+
+        return InteractionDataset(
+            dataset_name, data_dir, parallelizable=True, has_maps=True
+        )
+    
+    if "kitti" in dataset_name:
+        from trajdata.dataset_specific.kitti import KittiDataset
+
+        return KittiDataset(
+            dataset_name, data_dir, parallelizable=True, has_maps=False
+        )
+
+    raise ValueError(f"Dataset with name '{dataset_name}' is not supported")
+
+
+def get_raw_datasets(data_dirs: Dict[str, str]) -> List[RawDataset]:
+    raw_datasets: List[RawDataset] = list()
+
+    for dataset_name, data_dir in data_dirs.items():
+        raw_datasets.append(get_raw_dataset(dataset_name, data_dir))
+
+    return raw_datasets
