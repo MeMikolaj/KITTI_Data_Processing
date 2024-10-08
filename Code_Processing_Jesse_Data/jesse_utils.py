5d8b07730e081859e66ec0c52f1fd1b6120542a0
Mikolaj <126107752+MeMikolaj@users.noreply.github.com>
2024-10-08 16:22:01 +1100

    fixed acc vel and missing frames objects

--- Code_Processing_Jesse_Data/jesse_utils.py
+++ Code_Processing_Jesse_Data/jesse_utils.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 from scipy.spatial.transform import Rotation as R
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 ##############################################################################################################################
@@ -91,7 +93,10 @@ def local_to_global(folder_name, local_values, df_ego, category_dict):
     to_ret.insert(2, category_dict[str(int(local_values[1]))])
     return [folder_name] + to_ret
 
-def create_heading(df, drop_last = True):
+
+import numpy as np
+
+def create_heading(df, drop_last=True):
     # Sort the DataFrame by 'scene_id' and 'frame_id'
     df.sort_values(by=['scene_id', 'frame_id'], inplace=True)
     
@@ -101,22 +106,28 @@ def create_heading(df, drop_last = True):
     # Calculate 'heading' for each unique scene_id and object_id
     for scene_id in df['scene_id'].unique():
         scene_df = df[df['scene_id'] == scene_id]
-        
+
         for obj_id in scene_df['object_id'].unique():
             obj_df = scene_df[scene_df['object_id'] == obj_id]
+
+            # Store the last heading for continuity
+            last_heading = np.nan
             
             for i in range(len(obj_df) - 1):
-                current_index = obj_df.index[i]
-                next_index = obj_df.index[i + 1]
+                # Calculate the change in x and y
+                delta_x = obj_df['x'].iloc[i+1] - obj_df['x'].iloc[i]
+                delta_y = obj_df['y'].iloc[i+1] - obj_df['y'].iloc[i]
                 
-                # Calculate heading using atan2
-                heading_value = np.arctan2(
-                    df['y'].iloc[next_index] - df['y'].iloc[current_index], 
-                    df['x'].iloc[next_index] - df['x'].iloc[current_index] 
-                )
-                
-                # Assign the heading value to the current index
-                df.loc[current_index, 'heading'] = heading_value
+                # Check if the changes are less than 0.1
+                if abs(delta_x) < 0.1 and abs(delta_y) < 0.1 and i != 0:
+                    # Use the last heading if changes are small
+                    # Use the index from obj_df to find the correct row in df
+                    df.loc[obj_df.index[i], 'heading'] = last_heading
+                else:
+                    # Calculate heading using atan2
+                    heading_value = np.arctan2(delta_y, delta_x)
+                    df.loc[obj_df.index[i], 'heading'] = heading_value
+                    last_heading = heading_value  # Update last_heading
 
     # Drop the last values for each object where 'heading' is NaN
     if drop_last:
@@ -126,6 +137,7 @@ def create_heading(df, drop_last = True):
     df.reset_index(drop=True, inplace=True)
 
     return df
+
 
 
 #############################################################################################################################
@@ -185,7 +197,7 @@ def categ_to_vehicle(df, cat_list=['CAR', 'BUS', 'BICYCLE']):
 
 
 # Set df columns types to correct ones
-def set_df_types(df, object_id=False): 
+def set_df_types(df, include_obj_id=False): 
     # Dictionary mapping columns to their desired data types
     column_types = {
         'x': float,
@@ -209,7 +221,7 @@ def set_df_types(df, object_id=False):
             df[col] = df[col].astype(column_types[col])
     
     # object_id can be "ego" or an integer
-    if object_id:
+    if include_obj_id:
         df['object_id'] = df['object_id'].astype(int)
         
     return df
@@ -261,6 +273,107 @@ def add_vel_acc(df_object, df_motion):
     df_acc = pd.DataFrame(data_acc, columns=column_names_acc)
     
     return df_acc
+
+
+# Drop rows in a df with specified object id
+def remove_faulty_objects(df, dataset_name):
+    
+    FAULTY_OBJECTS = {
+        "0000": [],
+        "0001": [],
+        "0002": [8],
+        "0003": [],
+        "0004": [],
+        "0005": [],
+        "0006": [],
+        "0018": [],
+        "0020": [],
+    }
+    df = df[~df['object_id'].isin(FAULTY_OBJECTS[dataset_name])]
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+# Assigns new object ids to data that has missing frames (instead of object_id=1, have 1a, 1b, 1c...)
+def fix_missing_frames(df):
+    # Sort the DataFrame by object_id and frame_id
+    df = df.sort_values(by=['scene_id', 'object_id', 'frame_id']).reset_index(drop=True)
+
+    new_object_ids = []
+    current_suffix = 'a'  # Start with 'a' for the first segment
+
+    # Group by object_id
+    for object_id, group in df.groupby('object_id'):
+        last_frame = None  # To track the last frame in the sequence
+
+        for frame in group['frame_id']:
+            # Check if there is a gap in the sequence
+            if last_frame is not None and frame != last_frame + 1:
+                # If there's a gap, increment the suffix
+                current_suffix = chr(ord(current_suffix) + 1)  # Go to next letter
+            # Assign new object_id
+            new_id = f"{object_id}{current_suffix}"
+            new_object_ids.append(new_id)
+            last_frame = frame
+
+        # Reset the suffix after processing each object_id
+        current_suffix = 'a'
+
+    # Add the new object_id column to the original DataFrame
+    df['object_id'] = new_object_ids
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+#############################################################################################################################
+###############################################         Plotting Utils         ##############################################
+#############################################################################################################################
+
+# Plot Poses
+def plot_poses(df, output_path):
+    
+    unique_object_ids = df['object_id'].unique()
+
+    for object_id in unique_object_ids:
+        # Create a folder for an object
+        object_path = os.path.join(output_path, str(object_id))
+        maybe_makedirs(object_path)
+        
+        # Filter the DataFrame for the current object id
+        object_data = df[df['object_id'] == object_id]
+
+        # Sort by frame_id
+        object_data = object_data.sort_values(by='frame_id')
+
+        # Extract coordinates and heading
+        x = object_data['x'].values
+        y = object_data['y'].values
+        heading = object_data['heading'].values
+
+        for i in range(len(x)):
+            # Initialize the plot for the current frame
+            plt.figure(figsize=(10, 10))
+
+            # Plot the trajectory up to the current frame
+            plt.plot(x[:i+1], y[:i+1], label=f'Object ID: {object_id}', color='blue')
+
+            # Plot heading as an arrow to visualize heading for each frame
+            for j in range(i + 1):
+                plt.arrow(x[j], y[j], 0.5 * np.cos(heading[j]), 0.5 * np.sin(heading[j]),
+                        head_width=0.2, head_length=0.2, fc='red', ec='red', alpha=0.5)
+
+            # Customize the plot
+            plt.title(f'Object: {object_id} - Trajectory and Heading')
+            plt.xlabel('X Coordinate')
+            plt.ylabel('Y Coordinate')
+            plt.axis('equal')
+            plt.grid()
+
+            # Save the figure for the current frame
+            plot_file_path = os.path.join(object_path, f'id_{object_id}_frame_{object_data.iloc[i]["frame_id"]}_trajectory.png')
+            plt.savefig(plot_file_path)
+            plt.close()  # Close the figure to free memory
+            
+            
 
 
 #############################################################################################################################
--- Code_Processing_Jesse_Data/process_obj_global_jesse.py
+++ Code_Processing_Jesse_Data/process_obj_global_jesse.py
@@ -2,6 +2,7 @@
 Process KITTI Camera and Object poses into global coordinates. Save them in the csv files.
 Calculate the velcoties and accelerations from motion and add them to the object csv, save it.
 """
+import sys
 
 import os
 from tqdm import tqdm
@@ -9,15 +10,18 @@ import numpy as np
 import pandas as pd
 from scipy.spatial.transform import Rotation as R
 from jesse_utils import *
+from typing import Final, List, Dict
 
 
 base_path = '/home/mikolaj@acfr.usyd.edu.au/datasets/KITTI/Jesse_kitti'
 data_folders = os.listdir(base_path)
 
-output_path = '/home/mikolaj@acfr.usyd.edu.au/datasets/KITTI/Jesse_processed'
+output_path = '/home/mikolaj@acfr.usyd.edu.au/datasets/KITTI/Jesse_processed/data'
 category_path = '/home/mikolaj@acfr.usyd.edu.au/datasets/KITTI/raw'
 
-def process_data():
+output_path_plots = '/home/mikolaj@acfr.usyd.edu.au/datasets/KITTI/Jesse_processed/plots'
+
+def process_data(plot_trajectories=True):
     """ Take 3 csv files (camera pose, object pose, object motion) and arguments.
         Change data to XYZ convention, generate plots etc
 
@@ -25,9 +29,18 @@ def process_data():
         name (type): description
     """
     
+    print(f"""
+          ''''''''''''''''''''''''''''''''''''''''''''\n
+          - Heading the same as previous one if x < 0.1 and y < 0.1\n
+          - Agents with missing frames treated as many objects (1 -> 1a, 1b, 1c)\n
+          - Plot Trajectories: {plot_trajectories}\n
+          ''''''''''''''''''''''''''''''''''''''''''''
+          """)
+    
+    
+    
     # For each dataset process the data
-    for folder_name in tqdm(data_folders, desc="Processing Jesse's data from various datasets"):
-        print(folder_name)
+    for folder_name in tqdm(data_folders, desc="Processing datasets"):
         
         # Get dataset name
         dataset_name = folder_name.split('_')[1] # 0000 from kitti_0000 or 0006 from kitti_0006
@@ -51,14 +64,16 @@ def process_data():
         ####################### CAMERA POSE CV TO NORMAL TO CSV FILE #######################
 
         df_cmr = camera_to_normal_3D(df_cmr_pose, dataset_name) # CV to Normal
-        df_cmr = categ_to_vehicle(df_cmr)                       # Categ -> Vehicle
-        df_cmr = set_df_types(df_cmr)                           # Casting columns to their type
+        df_cmr = categ_to_vehicle(df_cmr)                       # Category (bus, car, bike) -> Vehicle
+        df_cmr = set_df_types(df_cmr, include_obj_id=False)     # Casting columns to their type
+        
 
-        df_camera_save = create_heading(df=df_cmr, drop_last=True) # Add heading column
+        df_cmr = create_heading(df=df_cmr, drop_last=False) # Add heading column
         
         # Save Data
         csv_file_path = os.path.join(output_path, dataset_name, 'camera_pose.csv')
-        df_camera_save.to_csv(csv_file_path, index=False)
+        df_cmr.dropna(inplace=True)
+        df_cmr.to_csv(csv_file_path, index=False)
         
         ####################### GET OBJECT CATEGORIES #######################
         
@@ -73,30 +88,33 @@ def process_data():
         ####################### OBJECT POSE CV TO NORMAL #######################
 
         df_obj = object_to_normal_3D(df_obj_pose, category_dict, dataset_name) # CV to Normal
-        df_obj = categ_to_vehicle(df_obj)                                      # Categ -> Vehicle
-        df_obj = set_df_types(df_obj, True)                                    # Casting columns to their type
+        df_obj = categ_to_vehicle(df_obj)                                      # Category (bus, car, bike) -> Vehicle
+        df_obj = set_df_types(df_obj, include_obj_id=True)                     # Casting columns to their type
         
-        df_obj = create_heading(df=df_obj, drop_last=False)
-        
-        # Save Data
-        csv_file_path = os.path.join(output_path, dataset_name, 'object_pose.csv')
-        df_obj.to_csv(csv_file_path, index=False)
-        
-        df_obj = df_obj.drop(columns=['heading']).copy()
+        # df_obj = create_heading(df=df_obj, drop_last=False)
 
+        # Save Data
+        # csv_file_path = os.path.join(output_path, dataset_name, 'object_pose.csv')
+        # df_obj.to_csv(csv_file_path, index=False)
+
+        # df_obj = df_obj.drop(columns=['heading']).copy()
+        
         ####################### OBJECT MOTION INCLUSIVE #######################
         
         # Cv to normal
         df_motion_pose = motion_to_normal_3D(df_obj_motion, category_dict, dataset_name) # CV to Normal
-        df_motion_pose = set_df_types(df_motion_pose, True)                              # Casting columns to their type
-
+        df_motion_pose = set_df_types(df_motion_pose, include_obj_id=True)               # Casting columns to their type
+        
         # Get Velocity and Acceleration from motion
-        df_acc = add_vel_acc(df_obj, df_motion_pose) # Vel and Acc                 ----- FIX ACC AND VEL where NAN is. Something is off
+        df_acc = add_vel_acc(df_obj, df_motion_pose)        # Vel and Acc
         
         # Improve the data
-        df_acc = categ_to_vehicle(df_acc)            # Categ -> Vehicle
-        df_acc = set_df_types(df_acc, True)          # Casting columns to their type
-        df_acc = create_heading(df_acc, drop_last=False)              # Add heading column
+        df_acc = categ_to_vehicle(df_acc)                   # Category (bus, car, bike) -> Vehicle
+        df_acc = set_df_types(df_acc, include_obj_id=True)  # Casting columns to their type
+        
+        df_acc.dropna(inplace=True)
+        df_acc = fix_missing_frames(df_acc)                 # Update object_id considering missing frames
+        df_acc = create_heading(df_acc, drop_last=False)    # Add heading column
         
         # Change the order
         new_order = ['scene_id', 'frame_id', 'object_id', 'category', 'x', 'y', 'z', 'heading', 'vx', 'vy', 'ax', 'ay']
@@ -105,7 +123,14 @@ def process_data():
         
         # Save Data
         csv_file_path = os.path.join(output_path, dataset_name, 'object_pose_motion.csv')
+        df_acc.dropna(inplace=True)
         df_acc.to_csv(csv_file_path, index=False)
+        
+        if plot_trajectories:
+            plot_poses(df_cmr, os.path.join(output_path_plots, dataset_name)) # Camera
+            plot_poses(df_acc, os.path.join(output_path_plots, dataset_name, 'objects')) # Motion
+            
+        # sys.exit()
         
 if __name__ == '__main__':
     process_data()
